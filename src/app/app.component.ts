fd7e0aea74b053dba983c074c3191cb90ffddb17
rafimarc101 <48298709+rafimarc101@users.noreply.github.com>
2019-03-26 14:38:23 +1300

    navigation & layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OrmocIMS';
+  mobHeight: any;
+  mobWidth: any;
+  constructor(private router: Router) {
+    this.mobHeight = (window.screen.height) + 'px';
+    this.mobWidth = (window.screen.width) + 'px';
+    console.log(this.mobHeight);
+    console.log(this.mobWidth);
+  }
 }
+
